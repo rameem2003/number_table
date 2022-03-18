c6912a08b81f39259c7388138505751a8d766a50
rameem2003 <rameem2019@gmail.com>
2022-03-18 22:06:21 +0600

    Add project files.

---
+++ number_table/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace table_of_number
+{
+    class app
+    {
+        public static void Main()
+        {
+            Console.Write("Enter the number: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int i = 1;
+            int product;
+
+            for(i = 1; i <= 10; i++)
+            {
+                product = n * i;
+                Console.WriteLine("Product of " + n + " * " + i + " is " + product);
+            }
+        }
+    }
+}
