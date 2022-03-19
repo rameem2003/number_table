270aedb9c73a58bf380f0401febd2da10a4aaf01
rameem2003 <rameem2019@gmail.com>
2022-03-19 08:41:54 +0600

    added new feature

--- number_table/Program.cs
+++ number_table/Program.cs
@@ -6,13 +6,17 @@ namespace table_of_number
     {
         public static void Main()
         {
-            Console.Write("Enter the number: ");
+            Console.WriteLine("<<<< Multiplication Table >>>>");
+            Console.Write("Enter the number for know the multiplication table: ");
             int n = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the range for multiplication table: ");
+            int range = Convert.ToInt32(Console.ReadLine());
 
             int i = 1;
             int product;
 
-            for(i = 1; i <= 10; i++)
+            for(i = 1; i <= range; i++)
             {
                 product = n * i;
                 Console.WriteLine("Product of " + n + " * " + i + " is " + product);
